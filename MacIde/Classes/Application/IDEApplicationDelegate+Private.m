7c5a68ea625953fdcf2b7976a9440d3dad27ae84
krishpranav <krisna.pranav@gmail.com>
2021-06-01 16:45:42 +0530

    [+] IDE Application Delegate [+]

--- MacIde/Classes/Application/IDEApplicationDelegate+Private.m
+++ MacIde/Classes/Application/IDEApplicationDelegate+Private.m
@@ -6,4 +6,43 @@
 //  Copyright © 2021 Krisna Pranav. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "IDEApplicationDelegate+Private.h"
+#import "IDEPreferences.h"
+#import "IDEColorTheme.h"
+#import "IDEMainWindowController.h"
+
+
+@implementation IDEApplicationDelegate(Private)
+
+- ( void )installApplicationSupportFiles
+{
+    void ( ^ installDir )( NSString * directory );
+    
+    installDir = ^( NSString * directory )
+    {
+        NSString * path;
+        NSString * bundlePath;
+        NSString * dirPath;
+        
+        path = [ FILE_MANAGER applicationSupportDirectory ];
+        
+        if( path == nil )
+        {
+            return;
+        }
+        
+        bundlePath = [ BUNDLE pathForResource: directory ofType: nil ];
+        dirPath    = [ path stringByAppendingPathComponent: [ bundlePath lastPathComponent ] ];
+        
+        if( [ FILE_MANAGER fileExistsAtPath: dirPath ] == NO )
+        {
+            [ FILE_MANAGER copyItemAtPath: bundlePath toPath: dirPath error: NULL ];
+        }
+    };
+    
+    installDir( @"Licenses" );
+    installDir( @"Templates" );
+    installDir( @"Themes" );
+}
+
+@end
