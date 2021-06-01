7ea45b6f0a8271cf94eeadd75aeea97a1fcbbaaf
krishpranav <krisna.pranav@gmail.com>
2021-06-01 13:07:33 +0530

    [+] Adding files [+]

---
+++ MacIde/Clang.bundle/Contents/Resources/include/ammintrin.h
@@ -0,0 +1,13 @@
+//
+//  ammintrin.h
+//  MacIde
+//
+//  Created by Elangovan Ayyasamy on 01/06/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#ifndef ammintrin_h
+#define ammintrin_h
+
+
+#endif /* ammintrin_h */
