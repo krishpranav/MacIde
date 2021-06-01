3db4d9900794cb0ab615a840701ad219f5c1b427
krishpranav <krisna.pranav@gmail.com>
2021-06-01 13:13:36 +0530

    [+] Adding files [+]

---
+++ MacIde/Clang.bundle/Contents/Resources/include/avxintrin.h
@@ -0,0 +1,13 @@
+//
+//  avxintrin.h
+//  MacIde
+//
+//  Created by Elangovan Ayyasamy on 01/06/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#ifndef avxintrin_h
+#define avxintrin_h
+
+
+#endif /* avxintrin_h */
---
+++ MacIde/Clang.bundle/Contents/Resources/include/bmi2intrin.h
@@ -0,0 +1,13 @@
+//
+//  bmi2intrin.h
+//  MacIde
+//
+//  Created by Elangovan Ayyasamy on 01/06/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#ifndef bmi2intrin_h
+#define bmi2intrin_h
+
+
+#endif /* bmi2intrin_h */
