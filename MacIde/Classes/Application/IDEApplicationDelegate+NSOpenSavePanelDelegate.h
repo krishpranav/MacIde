aa2f9e784641fc77138a9025d98a6925be1edbc3
krishpranav <krisna.pranav@gmail.com>
2021-06-01 16:14:52 +0530

    [+] Application updated [+]

---
+++ MacIde/Classes/Application/IDEApplicationDelegate+NSOpenSavePanelDelegate.h
@@ -0,0 +1,13 @@
+//
+//  IDEApplicationDelegate+NSOpenSavePanelDelegate.h
+//  MacIde
+//
+//  Created by Elangovan Ayyasamy on 01/06/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#ifndef IDEApplicationDelegate_NSOpenSavePanelDelegate_h
+#define IDEApplicationDelegate_NSOpenSavePanelDelegate_h
+
+
+#endif /* IDEApplicationDelegate_NSOpenSavePanelDelegate_h */
