029a35c79c0ea5c1ffe8f1be3e87a5c8d5aac39b
krishpranav <krisna.pranav@gmail.com>
2021-06-01 13:05:16 +0530

    [+] added some static values [+]

--- MacIde/Clang.bundle/Contents/Resources/include/altivec.h
+++ MacIde/Clang.bundle/Contents/Resources/include/altivec.h
@@ -19,3 +19,7 @@
 #define __CR6_LT     2
 #define __CR6_LT_REV 3
 
+#define __ATTRS_o_ai __attribute__((__overloadable__, __always_inline__))
+
+static vector signed char __ATTRS_o_ai
+vec_perm(vector signed char __a, vector signed char __b, vector unsigned char __c);
