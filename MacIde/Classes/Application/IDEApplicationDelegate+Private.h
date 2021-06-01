fe2418441aeeb39d2cd45716005dcd28105001e8
krishpranav <krisna.pranav@gmail.com>
2021-06-01 16:38:10 +0530

    [+] Adding files [+]

---
+++ MacIde/Classes/Application/IDEApplicationDelegate+Private.h
@@ -0,0 +1,13 @@
+//
+//  IDEApplicationDelegate+Private.h
+//  MacIde
+//
+//  Created by Elangovan Ayyasamy on 01/06/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#ifndef IDEApplicationDelegate_Private_h
+#define IDEApplicationDelegate_Private_h
+
+
+#endif /* IDEApplicationDelegate_Private_h */
---
+++ MacIde/Classes/Application/IDEApplicationDelegate.h
@@ -0,0 +1,13 @@
+//
+//  IDEApplicationDelegate.h
+//  MacIde
+//
+//  Created by Elangovan Ayyasamy on 01/06/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#ifndef IDEApplicationDelegate_h
+#define IDEApplicationDelegate_h
+
+
+#endif /* IDEApplicationDelegate_h */
