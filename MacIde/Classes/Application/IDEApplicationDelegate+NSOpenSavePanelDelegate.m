36159d3b09806ab8e389f26c1c54d568ba47a6b7
krishpranav <krisna.pranav@gmail.com>
2021-06-01 16:17:48 +0530

    [+] IDE Application Delegate NS Open [+]

--- MacIde/Classes/Application/IDEApplicationDelegate+NSOpenSavePanelDelegate.m
+++ MacIde/Classes/Application/IDEApplicationDelegate+NSOpenSavePanelDelegate.m
@@ -6,4 +6,29 @@
 //  Copyright © 2021 Krisna Pranav. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "IDEApplicationDelegate+NSOpenSavePanelDelegate.h"
+#import "IDEDocument.h"
+
+
+@implementation CEApplicationDelegate( NSOpenSavePanelDelegate )
+
+- ( BOOL )panel: ( id )sender validateURL: ( NSURL * )url error: ( NSError ** )outError
+{
+    CEDocument * document;
+    
+    ( void )sender;
+    ( void )outError;
+    
+    document = [ CEDocument documentWithPath: [ url path ] ];
+    
+    if( document.sourceFile.text == nil )
+    {
+        NSBeep();
+        
+        return NO;
+    }
+    
+    return YES;
+}
+
+@end
