be038620cc902c3c6495ce45d29c0c0d05a25c0e
krishpranav <krisna.pranav@gmail.com>
2021-06-01 16:13:14 +0530

    [+] Ide About Window Controller obj [+]

--- MacIde/Classes/About/IDEAboutWindowController.m
+++ MacIde/Classes/About/IDEAboutWindowController.m
@@ -24,4 +24,30 @@
     [super dealloc];
 }
 
+- ( void )awakeFromNib
+{
+    NSString * version;
+    NSString * build;
+    NSString * versionString;
+    NSImage  * icon;
+    CGImageRef cgImage;
+    NSRect     rect;
+    
+    version = [ [ NSBundle mainBundle ] objectForInfoDictionaryKey: NSBundleInfoKeyCFBundleShortVersionString ];
+    build   = [ [ NSBundle mainBundle ] objectForInfoDictionaryKey: NSBundleInfoKeyCFBundleVersion ];
+    
+    versionString = [ NSString stringWithFormat:  L10N( "AppVersion" ), version, [ build uppercaseString ] ];
+    
+    [ _versionTextField setStringValue: versionString ];
+    [ _backgroundView setBackgroundColor: [ NSColor whiteColor ] ];
+    
+    icon    = [ NSImage imageNamed: @"Application" ];
+    rect    = NSMakeRect( ( CGFloat )0, ( CGFloat )0, ( CGFloat )512, ( CGFloat )512 );
+    cgImage = [ icon CGImageForProposedRect: &rect context: nil hints: nil ];
+    icon   = [ [ NSImage alloc ] initWithCGImage: cgImage size: NSMakeSize( ( CGFloat )512, ( CGFloat )512 ) ];
+    
+    [ _iconView setImage: icon ];
+    [ icon release ];
+}
+
 @end
