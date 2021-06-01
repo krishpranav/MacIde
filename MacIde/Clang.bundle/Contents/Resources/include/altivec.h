7992e04ea8d99607e3278e0456ebccf91f3c183e
krishpranav <krisna.pranav@gmail.com>
2021-06-01 13:03:34 +0530

    [+] define values [+]

---
+++ MacIde/Clang.bundle/Contents/Resources/include/altivec.h
@@ -0,0 +1,21 @@
+//
+//  altivec.h
+//  MacIde
+//
+//  Created by Elangovan Ayyasamy on 01/06/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#ifndef __ALTIVEC_H
+#define __ALTIVEC_H
+
+#ifndef __ALTIVEC__
+#error "AltiVec support not enabled"
+#endif
+
+
+#define __CR6_EQ     0
+#define __CR6_EQ_REV 1
+#define __CR6_LT     2
+#define __CR6_LT_REV 3
+
