d7b591f158643c75082e93da1372ca1762989bbb
krishpranav <krisna.pranav@gmail.com>
2021-06-01 17:01:23 +0530

    [+] IDE Application Delegate classes [+]

--- MacIde/Classes/Application/IDEApplicationDelegate.m
+++ MacIde/Classes/Application/IDEApplicationDelegate.m
@@ -18,14 +18,13 @@
 #import "IDEDocument.h"
 #import "IDERegistrationWindowController.h"
 
-
 @implementation IDEApplicationDelegate
 
 @synthesize activeMainWindowController = _activeMainWindowController;
 
-+ ( IDEApplicationDelegate *) sharedInstance
++ ( CEApplicationDelegate * )sharedInstance
 {
-    return (IDEApplicationDelegate *)(APPLICATION.delegate);
+    return ( IDEApplicationDelegate * )( APPLICATION.delegate );
 }
 
 - ( id )init
@@ -42,7 +41,7 @@
     return self;
 }
 
-- (void)dealloc
+- ( void )dealloc
 {
     [ NOTIFICATION_CENTER removeObserver: self ];
     
@@ -71,4 +70,176 @@
         [ self newWindow: nil ];
     }
 }
+
+- ( void )applicationWillTerminate: ( NSNotification * )notification
+{
+    ( void )notification;
+    
+    [ [ IDEPreferences sharedInstance ] setFirstLaunch: NO ];
+}
+
+- ( IBAction )showPreferencesWindow: ( id )sender
+{
+    ( void )sender;
+    
+    if( _preferencesWindowController == nil )
+    {
+        _preferencesWindowController = [ IDEPreferencesWindowController new ];
+    }
+    
+    [ APPLICATION runModalForWindow: _preferencesWindowController.window ];
+}
+
+- ( IBAction )showAboutWindow: ( id )sender
+{
+    if( _aboutWindowController == nil )
+    {
+        _aboutWindowController = [ IDEAboutWindowController new ];
+    }
+    
+    [ _aboutWindowController.window center ];
+    [ _aboutWindowController showWindow: sender ];
+    [ _aboutWindowController.window makeKeyAndOrderFront: sender ];
+}
+
+- ( IBAction )showAlternateAboutWindow: ( id )sender
+{
+    if( _alternateAboutWindowController == nil )
+    {
+        _alternateAboutWindowController = [ IDEAlternateAboutWindowController new ];
+    }
+    
+    [ _alternateAboutWindowController.window center ];
+    [ _alternateAboutWindowController showWindow: sender ];
+    [ _alternateAboutWindowController.window makeKeyAndOrderFront: sender ];
+}
+
+- ( IBAction )showRegistrationWindow: ( id )sender
+{
+    ( void )sender;
+    
+    if( _registrationWindowController == nil )
+    {
+        _registrationWindowController = [ IDERegistrationWindowController new ];
+    }
+    
+    [ _registrationWindowController.window center ];
+    [ APPLICATION runModalForWindow: _registrationWindowController.window ];
+}
+
+- ( IBAction )newWindow: ( id )sender
+{
+    CEMainWindowController * controller;
+    
+    controller = [ CEMainWindowController new ];
+    
+    [ _mainWindowControllers addObject: controller ];
+    
+    [ controller.window center ];
+    [ controller showWindow: sender ];
+    [ controller.window makeKeyAndOrderFront: sender ];
+}
+
+- ( IBAction )newDocument: ( id )sender
+{
+    ( void )sender;
+    
+    if(_mainWindowControllers.count > 0 )
+    {
+        [ self.activeMainWindowController newDocument: sender ];
+    }
+    else
+    {
+        [ self newWindow: sender ];
+    }
+}
+
+- ( IBAction )openDocument: ( id )sender
+{
+    NSOpenPanel            * panel;
+    __block CEMainWindowController * controller;
+    
+    if( _mainWindowControllers.count > 0 )
+    {
+        controller = self.activeMainWindowController;
+        
+        [ controller.window makeKeyAndOrderFront: sender ];
+        [ controller showWindow: sender ];
+        [ controller openDocument: sender ];
+        
+        return;
+    }
+    
+    panel                                   = [ NSOpenPanel openPanel ];
+    panel.allowsMultipleSelection           = NO;
+    panel.canChooseDirectories              = NO;
+    panel.canChooseFiles                    = YES;
+    panel.canCreateDirectories              = NO;
+    panel.treatsFilePackagesAsDirectories   = YES;
+    panel.delegate                          = self;
+    
+    [ panel beginWithCompletionHandler: ^( NSInteger result )
+     {
+         if( result != NSFileHandlingPanelOKButton )
+         {
+             return;
+         }
+         
+         controller                  = [ IDEMainWindowController new ];
+         controller.activeDocument   = [ IDEDocument documentWithPath: [ panel.URL path ] ];
+         
+         [ _mainWindowControllers addObject: controller ];
+         
+         [ controller.window center ];
+         [ controller showWindow: sender ];
+         [ controller.window makeKeyAndOrderFront: sender ];
+     }
+     ];
+}
+
+- ( BOOL )application: ( NSApplication * )application openFile: ( NSString * )filename
+{
+    IDEMainWindowController * controller;
+    IDEDocument             * document;
+    
+    ( void )application;
+    
+    document = [ IDEDocument documentWithPath: filename ];
+    
+    if( document.sourceFile.text == nil )
+    {
+        return NO;
+    }
+    
+    if( _mainWindowControllers.count > 0 )
+    {
+        controller = self.activeMainWindowController;
+    }
+    else
+    {
+        controller = [ IDEMainWindowController new ];
+        
+        [ _mainWindowControllers addObject: controller ];
+        [ controller.window center ];
+    }
+    
+    [ controller setActiveDocument: document ];
+    [ controller showWindow: nil ];
+    [ controller.window makeKeyAndOrderFront: nil ];
+    
+    return YES;
+}
+
+- ( IBAction )resetColorThemes: ( id )sender
+{
+    NSString * path;
+    
+    ( void )sender;
+    
+    path = [ [ FILE_MANAGER applicationSupportDirectory ] stringByAppendingPathComponent: @"Themes" ];
+    
+    [ FILE_MANAGER removeItemAtPath: path error: NULL ];
+    [ self installApplicationSupportFiles ];
+}
+
 @end
