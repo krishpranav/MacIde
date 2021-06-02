8c8535f7cf2728c1810896732befc41df824c648
krishpranav <krisna.pranav@gmail.com>
2021-06-02 13:10:43 +0530

    [+] Adding files [+]

---
+++ MacIde/Classes/Categories/NSFileManager+CE.h
@@ -0,0 +1,13 @@
+//
+//  NSFileManager+CE.h
+//  MacIde
+//
+//  Created by Elangovan Ayyasamy on 02/06/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#ifndef NSFileManager_CE_h
+#define NSFileManager_CE_h
+
+
+#endif /* NSFileManager_CE_h */
---
+++ MacIde/Classes/Categories/NSImage+CE.h
@@ -0,0 +1,13 @@
+//
+//  NSImage+CE.h
+//  MacIde
+//
+//  Created by Elangovan Ayyasamy on 02/06/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#ifndef NSImage_CE_h
+#define NSImage_CE_h
+
+
+#endif /* NSImage_CE_h */
