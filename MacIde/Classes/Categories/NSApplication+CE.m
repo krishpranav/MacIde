6fecfdb11c0b278a110bd105f20e8389e4477bf6
krishpranav <krisna.pranav@gmail.com>
2021-06-02 13:07:55 +0530

    [+] NS Application CE [+]

--- MacIde/Classes/Categories/NSApplication+CE.m
+++ MacIde/Classes/Categories/NSApplication+CE.m
@@ -11,8 +11,76 @@
 
 static id < QLPreviewItem > __quickLookPreviewItem = nil;
 
-static void __exit(void) __attribute__((destructor));
-static void __exit(void)
+static void __exit( void ) __attribute__( ( destructor ) );
+static void __exit( void )
 {
-    RELEASE_IVAR(__quickLookPreviewItem);
+    RELEASE_IVAR( __quickLookPreviewItem );
 }
+
+@implementation NSApplication( IDE )
+
+- ( void )showQuickLookPanelForItemAtPath: ( NSString * )path sender: ( id )sender
+{
+    RELEASE_IVAR( __quickLookPreviewItem );
+    
+    __quickLookPreviewItem = [ [ IDEQuickLookItem alloc ] initWithPath: path ];
+    
+    if( __quickLookPreviewItem != nil )
+    {
+        [ [ QLPreviewPanel sharedPreviewPanel ] updateController ];
+        [ [ QLPreviewPanel sharedPreviewPanel ] makeKeyAndOrderFront: sender ];
+    }
+    else
+    {
+        NSBeep();
+    }
+}
+
+- ( BOOL )acceptsPreviewPanelControl: ( QLPreviewPanel * )panel
+{
+    ( void )panel;
+    
+    return __quickLookPreviewItem != nil;
+}
+
+- ( void )beginPreviewPanelControl: ( QLPreviewPanel * )panel
+{
+    ( void )panel;
+    
+    [ panel setDataSource: self ];
+}
+
+- ( void )endPreviewPanelControl: ( QLPreviewPanel * )panel
+{
+    ( void )panel;
+    
+    [ panel setDataSource: nil ];
+    
+    RELEASE_IVAR( __quickLookPreviewItem );
+}
+
+- ( NSInteger )numberOfPreviewItemsInPreviewPanel: ( QLPreviewPanel * )panel
+{
+    ( void )panel;
+    
+    if( __quickLookPreviewItem != nil )
+    {
+        return 1;
+    }
+    
+    return 0;
+}
+
+- ( id < QLPreviewItem > )previewPanel: ( QLPreviewPanel * )panel previewItemAtIndex: ( NSInteger )index
+{
+    ( void )panel;
+    
+    if( index != 0 )
+    {
+        return nil;
+    }
+    
+    return __quickLookPreviewItem;
+}
+
+@end
