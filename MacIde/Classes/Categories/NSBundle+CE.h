9d852461adabca22ac77fdef4f0ab161bd523d04
krishpranav <krisna.pranav@gmail.com>
2021-06-02 13:08:38 +0530

    [+] NS Bundle CE [+]

--- MacIde/Classes/Categories/NSBundle+CE.h
+++ MacIde/Classes/Categories/NSBundle+CE.h
@@ -6,8 +6,24 @@
 //  Copyright © 2021 Krisna Pranav. All rights reserved.
 //
 
-#ifndef NSBundle_CE_h
-#define NSBundle_CE_h
+FOUNDATION_EXPORT NSString * const NSBundleInfoKeyCFBundleDevelopmentRegion;
+FOUNDATION_EXPORT NSString * const NSBundleInfoKeyCFBundleExecutable;
+FOUNDATION_EXPORT NSString * const NSBundleInfoKeyCFBundleIconFile;
+FOUNDATION_EXPORT NSString * const NSBundleInfoKeyCFBundleIdentifier;
+FOUNDATION_EXPORT NSString * const NSBundleInfoKeyCFBundleInfoDictionaryVersion;
+FOUNDATION_EXPORT NSString * const NSBundleInfoKeyCFBundleName;
+FOUNDATION_EXPORT NSString * const NSBundleInfoKeyCFBundlePackageType;
+FOUNDATION_EXPORT NSString * const NSBundleInfoKeyCFBundleShortVersionString;
+FOUNDATION_EXPORT NSString * const NSBundleInfoKeyCFBundleSignature;
+FOUNDATION_EXPORT NSString * const NSBundleInfoKeyCFBundleVersion;
+FOUNDATION_EXPORT NSString * const NSBundleInfoKeyLSApplicationCategoryType;
+FOUNDATION_EXPORT NSString * const NSBundleInfoKeyLSMinimumSystemVersion;
+FOUNDATION_EXPORT NSString * const NSBundleInfoKeyNSHumanReadableCopyright;
+FOUNDATION_EXPORT NSString * const NSBundleInfoKeyNSMainNibFile;
+FOUNDATION_EXPORT NSString * const NSBundleInfoKeyNSPrincipalClass;
+
+@interface NSBundle( IDE )
 
 
-#endif /* NSBundle_CE_h */
+
+@end
