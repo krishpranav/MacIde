13b6150fb16b2bbb450519bdfa289219b261ddd2
krishpranav <krisna.pranav@gmail.com>
2021-06-02 12:13:05 +0530

    [+] IDE Ordered Dictionary [+]

--- MacIde/Classes/Base/IDEOrderedDictionary.m
+++ MacIde/Classes/Base/IDEOrderedDictionary.m
@@ -8,7 +8,6 @@
 
 #import "IDEOrderedDictionary.h"
 
-
 @implementation IDEOrderedDictionary
 
 - ( id )initWithObjects: ( const id[] )objects forKeys: ( const id< NSCopying >[] )keys count: ( NSUInteger )count
@@ -22,3 +21,65 @@
     return self;
 }
 
+- ( NSUInteger )count
+{
+    return _keys.count;
+}
+
+- ( id )objectForKey: ( id )key
+{
+    NSUInteger i;
+    
+    i = [ _keys indexOfObject: key ];
+    
+    if( i == NSNotFound )
+    {
+        return nil;
+    }
+    
+    return [ _objects objectAtIndex: i ];
+}
+
+- ( NSEnumerator * )keyEnumerator
+{
+    return [ _keys objectEnumerator ];
+}
+
+- ( NSString * )descriptionWithLocale: ( id )locale
+{
+    NSString * description;
+    NSUInteger i;
+    
+    description = [ NSString stringWithFormat: @"%@ <%p> {\n", NSStringFromClass( [ self class ] ), self ];
+    
+    ( void )locale;
+    
+    for( i = 0; i < _keys.count; i++ )
+    {
+        description = [ description stringByAppendingFormat: @"\t%@ = %@;\n", [ _keys objectAtIndex: i ], [ _objects objectAtIndex: i ] ];
+    }
+    
+    return [ description stringByAppendingString: @"}" ];
+}
+
+- ( id )keyAtIndex: ( NSUInteger )index
+{
+    if( index >= _keys.count )
+    {
+        return nil;
+    }
+    
+    return [ _keys objectAtIndex: index ];
+}
+
+- ( id )objectAtIndex: ( NSUInteger )index
+{
+    if( index >= _keys.count )
+    {
+        return nil;
+    }
+    
+    return [ _objects objectAtIndex: index ];
+}
+
+@end
