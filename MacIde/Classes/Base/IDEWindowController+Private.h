66f61e52e863f8499a55363bd3262b25fb8b1aa3
krishpranav <krisna.pranav@gmail.com>
2021-06-02 12:47:49 +0530

    [+] Adding files [+]

---
+++ MacIde/Classes/Base/IDEWindowController+Private.h
@@ -0,0 +1,13 @@
+//
+//  IDEWindowController+Private.h
+//  MacIde
+//
+//  Created by Elangovan Ayyasamy on 02/06/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#ifndef IDEWindowController_Private_h
+#define IDEWindowController_Private_h
+
+
+#endif /* IDEWindowController_Private_h */
---
+++ MacIde/Classes/Base/IDEWindowController.h
@@ -0,0 +1,13 @@
+//
+//  IDEWindowController.h
+//  MacIde
+//
+//  Created by Elangovan Ayyasamy on 02/06/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#ifndef IDEWindowController_h
+#define IDEWindowController_h
+
+
+#endif /* IDEWindowController_h */
