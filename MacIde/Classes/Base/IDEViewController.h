7ee1bd4e5e5d79f267199eeabaea046ab979c4cf
krishpranav <krisna.pranav@gmail.com>
2021-06-02 12:20:19 +0530

    [+] Adding new files [+]

---
+++ MacIde/Classes/Base/IDEViewController.h
@@ -0,0 +1,13 @@
+//
+//  IDEViewController.h
+//  MacIde
+//
+//  Created by Elangovan Ayyasamy on 02/06/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#ifndef IDEViewController_h
+#define IDEViewController_h
+
+
+#endif /* IDEViewController_h */
---
+++ MacIde/Classes/Base/IDEViewController.m
@@ -0,0 +1,9 @@
+//
+//  IDEViewController.m
+//  MacIde
+//
+//  Created by Elangovan Ayyasamy on 02/06/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
