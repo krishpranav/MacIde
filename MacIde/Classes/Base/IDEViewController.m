515443d112f081b49ab34fb89e8dadaf20ba869c
krishpranav <krisna.pranav@gmail.com>
2021-06-02 12:24:49 +0530

    [+] IDE View controller updated [+]

--- MacIde/Classes/Base/IDEViewController.m
+++ MacIde/Classes/Base/IDEViewController.m
@@ -6,4 +6,69 @@
 //  Copyright © 2021 Krisna Pranav. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "IDEViewController.h"
+
+static NSString * const __classSuffix               = @"Controller";
+NSString * const IDEViewControllerException   = @"IDEViewControllerException";
+
+@implementation IDEViewController
+
+@synthesize popover = _popover;
+
+- ( id )init
+{
+    NSString * className;
+    NSString * nibName;
+    
+    className = NSStringFromClass( [ self class ] );
+    
+    if( [ className hasSuffix: __classSuffix ] == NO )
+    {
+        @throw [ NSException exceptionWithName: IDEViewControllerException reason: @"Invalid view controller class name" userInfo: nil ];
+    }
+    
+    nibName = [ className substringToIndex: className.length - __classSuffix.length ];
+    
+    if( ( self = [ super initWithNibName: nibName bundle: nil ] ) )
+    {}
+    
+    return self;
+}
+
+- ( void )dealloc
+{
+    if( _popover.shown == YES )
+    {
+        [ _popover close ];
+    }
+    
+    RELEASE_IVAR( _popover );
+    
+    [ super dealloc ];
+}
+
+- ( void )openInPopoverRelativeToRect: ( NSRect )rect ofView: ( NSView * )view preferredEdge: ( NSRectEdge )edge
+{
+    if( _popover.shown == YES )
+    {
+        return;
+    }
+    
+    RELEASE_IVAR( _popover );
+    
+    _popover                       = [ NSPopover new ];
+    _popover.behavior              = NSPopoverBehaviorTransient;
+    _popover.contentViewController = self;
+    
+    [ _popover showRelativeToRect: rect ofView: view preferredEdge: edge ];
+}
+
+- ( void )closePopover
+{
+    [ _popover close ];
+    
+    RELEASE_IVAR( _popover );
+}
+
+
+@end
