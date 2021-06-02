5e0a5f280bb4bf31d48e105418125f7cd8d2bf20
krishpranav <krisna.pranav@gmail.com>
2021-06-02 12:57:53 +0530

    [+] IDE Window Controller m [+]

--- MacIde/Classes/Base/IDEWindowController.m
+++ MacIde/Classes/Base/IDEWindowController.m
@@ -13,6 +13,7 @@ NSString * const IDEWindowControllerException = @"IDEWindowControllerException";
 
 @implementation IDEWindowController
 
+
 - ( id )init
 {
     NSString * className;
@@ -34,3 +35,37 @@ NSString * const IDEWindowControllerException = @"IDEWindowControllerException";
     
     return self;
 }
+
+- ( void )dealloc
+{
+    [ NOTIFICATION_CENTER removeObserver: self ];
+    
+    [ super dealloc ];
+}
+
+- ( BOOL )releaseOnWindowClose
+{
+    @synchronized( self )
+    {
+        return _releaseOnWindowClose;
+    }
+}
+
+- ( void )setReleaseOnWindowClose: ( BOOL )release
+{
+    @synchronized( self )
+    {
+        if( release == YES && _releaseOnWindowClose == NO )
+        {
+            [ self retain ];
+        }
+        else if( release == NO &&  _releaseOnWindowClose == YES )
+        {
+            [ self release ];
+        }
+        
+        _releaseOnWindowClose = release;
+    }
+}
+
+@end
