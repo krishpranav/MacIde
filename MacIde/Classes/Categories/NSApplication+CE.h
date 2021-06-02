1d06f481adadb447e0060169e2ed8c7186076a66
krishpranav <krisna.pranav@gmail.com>
2021-06-02 13:00:21 +0530

    [+] Adding Categories files [+]

---
+++ MacIde/Classes/Categories/NSApplication+CE.h
@@ -0,0 +1,13 @@
+//
+//  NSApplication+CE.h
+//  MacIde
+//
+//  Created by Elangovan Ayyasamy on 02/06/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#ifndef NSApplication_CE_h
+#define NSApplication_CE_h
+
+
+#endif /* NSApplication_CE_h */
---
+++ MacIde/Classes/Categories/NSBundle+CE.h
@@ -0,0 +1,13 @@
+//
+//  NSBundle+CE.h
+//  MacIde
+//
+//  Created by Elangovan Ayyasamy on 02/06/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#ifndef NSBundle_CE_h
+#define NSBundle_CE_h
+
+
+#endif /* NSBundle_CE_h */
