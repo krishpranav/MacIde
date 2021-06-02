2b671a16d253e0604ff54fbd52a3947982b97cf0
krishpranav <krisna.pranav@gmail.com>
2021-06-02 13:09:20 +0530

    [+] NS Budle CE [+]

--- MacIde/Classes/Categories/NSBundle+CE.m
+++ MacIde/Classes/Categories/NSBundle+CE.m
@@ -6,4 +6,26 @@
 //  Copyright © 2021 Krisna Pranav. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "NSBundle+CE.h"
+
+NSString * const NSBundleInfoKeyCFBundleDevelopmentRegion       = @"CFBundleDevelopmentRegion";
+NSString * const NSBundleInfoKeyCFBundleExecutable              = @"CFBundleExecutable";
+NSString * const NSBundleInfoKeyCFBundleIconFile                = @"CFBundleIconFile";
+NSString * const NSBundleInfoKeyCFBundleIdentifier              = @"CFBundleIdentifier";
+NSString * const NSBundleInfoKeyCFBundleInfoDictionaryVersion   = @"CFBundleInfoDictionaryVersion";
+NSString * const NSBundleInfoKeyCFBundleName                    = @"CFBundleName";
+NSString * const NSBundleInfoKeyCFBundlePackageType             = @"CFBundlePackageType";
+NSString * const NSBundleInfoKeyCFBundleShortVersionString      = @"CFBundleShortVersionString";
+NSString * const NSBundleInfoKeyCFBundleSignature               = @"CFBundleSignature";
+NSString * const NSBundleInfoKeyCFBundleVersion                 = @"CFBundleVersion";
+NSString * const NSBundleInfoKeyLSApplicationCategoryType       = @"LSApplicationCategoryType";
+NSString * const NSBundleInfoKeyLSMinimumSystemVersion          = @"LSMinimumSystemVersion";
+NSString * const NSBundleInfoKeyNSHumanReadableCopyright        = @"NSHumanReadableCopyright";
+NSString * const NSBundleInfoKeyNSMainNibFile                   = @"NSMainNibFile";
+NSString * const NSBundleInfoKeyNSPrincipalClass                = @"NSPrincipalClass";
+
+@implementation NSBundle( IDE )
+
+
+
+@end
