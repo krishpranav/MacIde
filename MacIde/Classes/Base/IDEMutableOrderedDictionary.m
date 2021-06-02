200bac9388ac67f0bac1af70baf73899cc381666
krishpranav <krisna.pranav@gmail.com>
2021-06-02 11:26:54 +0530

    [+] IDE Mutable Ordered Dicitonary updated [+]

--- MacIde/Classes/Base/IDEMutableOrderedDictionary.m
+++ MacIde/Classes/Base/IDEMutableOrderedDictionary.m
@@ -31,3 +31,87 @@
     
     return self;
 }
+
+- ( NSUInteger )count
+{
+    return _keys.count;
+}
+
+- ( id )objectForKey: ( id )key
+{
+    NSUInteger i;
+    
+    i = [ _keys indexOfObject: key ];
+    
+    if( i == NSNotFound )
+    {
+        return nil;
+    }
+    
+    return [ _objects objectAtIndex: i ];
+}
+
+- ( NSEnumerator * )keyEnumerator
+{
+    return [ _keys objectEnumerator ];
+}
+
+- ( void )setObject: ( id )object forKey: ( id< NSCopying > )key
+{
+    [ _keys    addObject: key ];
+    [ _objects addObject: object ];
+}
+
+- ( void )removeObjectForKey: ( id )key
+{
+    NSUInteger i;
+    
+    i = [ _keys indexOfObject: key ];
+    
+    if( i == NSNotFound )
+    {
+        return;
+    }
+    
+    [ _keys    removeObjectAtIndex: i ];
+    [ _objects removeObjectAtIndex: i ];
+}
+
+- ( NSString * )descriptionWithLocale: ( id )locale
+{
+    NSString * description;
+    NSUInteger i;
+    
+    description = [ NSString stringWithFormat: @"%@ <%p> {\n", NSStringFromClass( [ self class ] ), self ];
+    
+    ( void )locale;
+    
+    for( i = 0; i < _keys.count; i++ )
+    {
+        description = [ description stringByAppendingFormat: @"\t%@ = %@;\n", [ _keys objectAtIndex: i ], [ _objects objectAtIndex: i ] ];
+    }
+    
+    return [ description stringByAppendingString: @"}" ];
+}
+
+- ( id )keyAtIndex: ( NSUInteger )index
+{
+    if( index >= _keys.count )
+    {
+        return nil;
+    }
+    
+    return [ _keys objectAtIndex: index ];
+}
+
+- ( id )objectAtIndex: ( NSUInteger )index
+{
+    if( index >= _keys.count )
+    {
+        return nil;
+    }
+    
+    return [ _objects objectAtIndex: index ];
+}
+
+@end
