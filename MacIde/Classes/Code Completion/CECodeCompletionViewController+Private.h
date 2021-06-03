8632228d9854c6546e962cc1aeb6e39fca1deea3
krishpranav <krisna.pranav@gmail.com>
2021-06-03 13:51:35 +0530

    [+] Adding files [+]

---
+++ MacIde/Classes/Code Completion/CECodeCompletionViewController+Private.h
@@ -0,0 +1,13 @@
+//
+//  CECodeCompletionViewController+Private.h
+//  MacIde
+//
+//  Created by Elangovan Ayyasamy on 03/06/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#ifndef CECodeCompletionViewController_Private_h
+#define CECodeCompletionViewController_Private_h
+
+
+#endif /* CECodeCompletionViewController_Private_h */
---
+++ MacIde/Classes/Code Completion/CECodeCompletionViewController.h
@@ -0,0 +1,13 @@
+//
+//  CECodeCompletionViewController.h
+//  MacIde
+//
+//  Created by Elangovan Ayyasamy on 03/06/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#ifndef CECodeCompletionViewController_h
+#define CECodeCompletionViewController_h
+
+
+#endif /* CECodeCompletionViewController_h */
