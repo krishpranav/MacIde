8e624a464a8fc9d34f69d1038b5135d9f563fc7b
krishpranav <krisna.pranav@gmail.com>
2021-06-03 13:54:09 +0530

    [+] interface [+]

--- MacIde/Classes/Code Completion/CECodeCompletionViewController.h
+++ MacIde/Classes/Code Completion/CECodeCompletionViewController.h
@@ -6,8 +6,21 @@
 //  Copyright © 2021 Krisna Pranav. All rights reserved.
 //
 
-#ifndef CECodeCompletionViewController_h
-#define CECodeCompletionViewController_h
+#import "CEViewController.h"
+
+@interface CECodeCompletionViewController: CEViewController
+{
+@protected
+    
+    NSArray     * _results;
+    BOOL          _cancel;
+    BOOL          _isOpening;
+    NSTableView * _tableView;
+    
+@private
+    
+    RESERVED_IVARS( CECodeCompletionViewController, 5 );
+}
 
 
-#endif /* CECodeCompletionViewController_h */
+
