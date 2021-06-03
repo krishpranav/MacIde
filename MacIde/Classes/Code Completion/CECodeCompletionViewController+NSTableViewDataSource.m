fa745101497711ef4beb9d9023d8211ce47467a5
krishpranav <krisna.pranav@gmail.com>
2021-06-03 11:16:58 +0530

    [+] CE Code Completation View Controller [+]

--- MacIde/Classes/Code Completion/CECodeCompletionViewController+NSTableViewDataSource.m
+++ MacIde/Classes/Code Completion/CECodeCompletionViewController+NSTableViewDataSource.m
@@ -6,4 +6,44 @@
 //  Copyright © 2021 Krisna Pranav. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+
+#import "CECodeCompletionViewController+NSTableViewDelegate.h"
+
+@implementation CECodeCompletionViewController( NSTableViewDataSource )
+
+- ( NSInteger )numberOfRowsInTableView: ( NSTableView * )tableView
+{
+    ( void )tableView;
+    
+    return ( NSInteger )( _results.count );
+}
+
+- ( id )tableView: ( NSTableView * )tableView objectValueForTableColumn: ( NSTableColumn * )tableColumn row: ( NSInteger )row
+{
+    CKCompletionResult * result;
+    CKCompletionChunk  * chunk;
+    
+    ( void )tableView;
+    ( void )tableColumn;
+    
+    @try
+    {
+        result = [ _results objectAtIndex: ( NSUInteger )row ];
+        
+        for( chunk in result.chunks )
+        {
+            if( chunk.kind == CKCompletionChunkKindTypedText )
+            {
+                return chunk.text;
+            }
+        }
+    }
+    @catch ( NSException * e )
+    {
+        ( void )e;
+    }
+    
+    return nil;
+}
+
+@end
