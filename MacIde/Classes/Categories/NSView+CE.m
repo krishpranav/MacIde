4207fdc4571ce3e7c38aa4eb94a76d5ae6f0e352
krishpranav <krisna.pranav@gmail.com>
2021-06-03 10:32:19 +0530

    [+] NS VIEW CE [+]

--- MacIde/Classes/Categories/NSView+CE.m
+++ MacIde/Classes/Categories/NSView+CE.m
@@ -6,4 +6,27 @@
 //  Copyright © 2021 Krisna Pranav. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import "NSView+CE.h"
+
+@implementation NSView( CE )
+
+- ( void )centerInSuperview
+{
+    NSRect bounds;
+    NSRect frame;
+    
+    if( self.superview == nil )
+    {
+        return;
+    }
+    
+    bounds = self.superview.bounds;
+    frame  = self.frame;
+    
+    frame.origin.x = ( bounds.size.width  - frame.size.width  ) / ( CGFloat )2;
+    frame.origin.y = ( bounds.size.height - frame.size.height ) / ( CGFloat )2;
+    
+    self.frame = frame;
+}
+
+@end
