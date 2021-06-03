f6e1c047718f26bc082b0bcfd9ac48ff4c4772dd
krishpranav <krisna.pranav@gmail.com>
2021-06-03 13:59:15 +0530

    [+] Adding files [+]

---
+++ MacIde/Classes/Console/CEConsoleViewController+Private.h
@@ -0,0 +1,13 @@
+//
+//  CEConsoleViewController+Private.h
+//  MacIde
+//
+//  Created by Elangovan Ayyasamy on 03/06/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#ifndef CEConsoleViewController_Private_h
+#define CEConsoleViewController_Private_h
+
+
+#endif /* CEConsoleViewController_Private_h */
---
+++ MacIde/Classes/Console/CEConsoleViewController.h
@@ -0,0 +1,13 @@
+//
+//  CEConsoleViewController.h
+//  MacIde
+//
+//  Created by Elangovan Ayyasamy on 03/06/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#ifndef CEConsoleViewController_h
+#define CEConsoleViewController_h
+
+
+#endif /* CEConsoleViewController_h */
