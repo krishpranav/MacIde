07f5744b065e41d9c3b97f3fad13cd0c6264a4fc
krishpranav <krisna.pranav@gmail.com>
2021-06-03 11:14:32 +0530

    [+] Adding files [+]

---
+++ MacIde/Classes/Code Completion/CECodeCompletionViewController+NSTableViewDataSource.h
@@ -0,0 +1,13 @@
+//
+//  CECodeCompletionViewController+NSTableViewDataSource.h
+//  MacIde
+//
+//  Created by Elangovan Ayyasamy on 03/06/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#ifndef CECodeCompletionViewController_NSTableViewDataSource_h
+#define CECodeCompletionViewController_NSTableViewDataSource_h
+
+
+#endif /* CECodeCompletionViewController_NSTableViewDataSource_h */
---
+++ MacIde/Classes/Code Completion/CECodeCompletionViewController+NSTableViewDelegate.h
@@ -0,0 +1,13 @@
+//
+//  CECodeCompletionViewController+NSTableViewDelegate.h
+//  MacIde
+//
+//  Created by Elangovan Ayyasamy on 03/06/21.
+//  Copyright © 2021 Krisna Pranav. All rights reserved.
+//
+
+#ifndef CECodeCompletionViewController_NSTableViewDelegate_h
+#define CECodeCompletionViewController_NSTableViewDelegate_h
+
+
+#endif /* CECodeCompletionViewController_NSTableViewDelegate_h */
